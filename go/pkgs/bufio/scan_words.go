b007a4c8770bb6a8f023d0a3d0006cdde06e35b5
t-zeus <netsan@126.com>
2019-01-09 15:45:20 +0800

    scan words

---
+++ go/pkgs/bufio/scan_words.go
@@ -0,0 +1,21 @@
+package main
+
+import(
+    "bufio"
+    "strings"
+    "fmt"
+)
+
+func main() {
+    input := "foo bar   baz"
+    // strings.NewReader 创建 io.Reader 的实现
+    scanner := bufio.NewScanner(strings.NewReader(input))
+    // 设定分割函数，默认 bufio.ScanLine
+    scanner.Split(bufio.ScanWords)
+    // Scan 扫描是否终止（到达文本末端、遇到错误）
+    // 非终止，Scanner.Text() 有效
+    // 终止，Scanner.Err() 错误原因（非 EOF）
+    for scanner.Scan() {
+        fmt.Println(scanner.Text())
+    }
+}
