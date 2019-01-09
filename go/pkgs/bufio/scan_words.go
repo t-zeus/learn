87d05c52544af230f04b741e2f5a0a83f63c8c90
t-zeus <netsan@126.com>
2019-01-09 15:55:11 +0800

    add comment

--- go/pkgs/bufio/scan_words.go
+++ go/pkgs/bufio/scan_words.go
@@ -11,6 +11,7 @@ func main() {
     // strings.NewReader 创建 io.Reader 的实现
     scanner := bufio.NewScanner(strings.NewReader(input))
     // 设定分割函数，默认 bufio.ScanLine
+    // bufio.ScanWords 会把空格都去掉
     scanner.Split(bufio.ScanWords)
     // Scan 扫描是否终止（到达文本末端、遇到错误）
     // 非终止，Scanner.Text() 有效
